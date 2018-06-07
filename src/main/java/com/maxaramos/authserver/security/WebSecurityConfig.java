06ee788e89f04261e350ae788066a4d78c68289c
mramos <mramos@virtela.net>
2018-06-07 14:06:15 +0800

    Updated security logic.

--- src/main/java/com/maxaramos/authserver/security/WebSecurityConfig.java
+++ src/main/java/com/maxaramos/authserver/security/WebSecurityConfig.java
@@ -41,6 +41,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+//		AuthorizationEndpoint
+//		OAuth2AuthenticationProcessingFilter
 		http
 			.requestMatchers()
 				.antMatchers("/", "/login", "/oauth/authorize", "/oauth/confirm_access")
